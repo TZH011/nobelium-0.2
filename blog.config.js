b69dd7a7db56a51a8c0f4afee73aeabff7cdbb07
TZH001 <22451688@qq.com>
2021-05-24 02:38:18 +0800

    Initial commit

---
+++ blog.config.js
@@ -0,0 +1,61 @@
+const BLOG = {
+  title: 'Snoword Blog',
+  author: 'Snoword',
+  email: '33chijian@gmail.com',
+  link: 'https://snoword.xyz',
+  description: '这是一个分享想法，记录技术的个人博客。',
+  lang: 'zh-CN',
+  appearance: 'auto', // ['light', 'dark', 'auto'],
+  font: 'sans-serif', // ['sans-serif', 'serif']
+  lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
+  darkBackground: '#111827', // use hex value, don't forget '#'
+  path: '', // leave this empty unless you want to deploy Nobelium in a folder
+  since: 2020, // if leave this empty, current year will be used.
+  postsPerPage: 7,
+  sortByDate: true,
+  showAbout: true, // WIP
+  showArchive: true, // WIP
+  autoCollapsedNavBar: false, // the automatically collapsed navigation bar
+  socialLink: 'https://twitter.com/snoword1024',
+  seo: {
+    keywords: ['Blog', 'Website', 'Notion'],
+    googleSiteVerification: 'beUFeNNyZVeRqPbjnjIDdt6d6EFxtwYhAcynXXLE9u0' // Remove the value or replace it with your own google site verification code
+  },
+  notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
+  // notionPageId: '0102aa04979740b5a2df0bd2aa272af5', // DO NOT CHANGE THIS！！！
+  notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
+  analytics: {
+    provider: 'ga', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
+    ackeeConfig: {
+      tracker: '', // e.g 'https://ackee.craigary.net/tracker.js'
+      dataAckeeServer: '', // e.g https://ackee.craigary.net , don't end with a slash
+      domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
+    },
+    gaConfig: {
+      measurementId: 'G-BGZTQRN52S' // e.g: G-XXXXXXXXXX
+    }
+  },
+  comment: {
+    // support provider: gitalk, utterances, cusdis
+    provider: 'gitalk', // leave it empty if you don't need any comment plugin
+    gitalkConfig: {
+      repo: 'Resume', // The repository of store comments
+      owner: 'snowords',
+      admin: ['snowords'],
+      clientID: 'e608fba0dff44a15ed1a',
+      clientSecret: '4a7021f0867c0fef3608463089918387b2001d51',
+      distractionFreeMode: false
+    },
+    utterancesConfig: {
+      repo: ''
+    },
+    cusdisConfig: {
+      appId: '048128b5-ea56-41bf-b035-9d95fa314c65', // data-app-id
+      host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
+      scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
+    }
+  },
+  isProd: process.env.VERCEL_ENV === 'production' // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+}
+// export default BLOG
+module.exports = BLOG
---
+++ components/Footer.js
@@ -0,0 +1,30 @@
+import BLOG from '@/blog.config'
+// import Vercel from '@/components/Vercel'
+const Footer = ({ fullWidth }) => {
+  const d = new Date()
+  const y = d.getFullYear()
+  const from = +BLOG.since
+  return (
+    <div
+      className={`mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all ${
+        !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
+      }`}
+    >
+      <hr className="border-gray-200 dark:border-gray-600" />
+      <div className="my-4 text-sm leading-6 font-medium">
+        <div className="flex align-baseline justify-between flex-wrap">
+          <p>
+           {BLOG.author} © {from === y || !from ? y : `${from} - ${y}`}
+          </p>
+          <p>
+            Powered by <a className="hover:text-blue-600" href="https://github.com/craigary/nobelium" target="_blank" rel="noreferrer">Nobelium</a>, 
+            <a className="hover:text-blue-600" href="https://reactjs.org/" target="_blank" rel="noreferrer">React</a>, 
+            <a className="hover:text-blue-600" href="https://nextjs.org/" target="_blank" rel="noreferrer">Next</a> and <a className="hover:text-blue-600" href="https://tailwindcss.com/" target="_blank" rel="noreferrer">Tailwind CSS</a>.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
